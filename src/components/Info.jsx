cc48417a5f2485c5186e5aae9d1fc490fed65562
ReshanCSX <reshan.luck@gmail.com>
2023-07-23 15:28:14 +0530

    Added click events to buttons

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -3,14 +3,25 @@ import email_icon from '../images/email-icon.png'
 import linkedin_icon from '../images/linkedin-icon.png'
 
 export default function Info(){
+
+    function mailHandler(){
+        window.location.href = 'mailto:reshan@example.com';
+    }
+
+    function linkedInHandler(){
+        window.open('https://www.linkedin.com/in/reshancsx/', '_blank');
+    }
+      
+
     return(
+
         <header>
             <img src={profile_image} />
             <h1>ReshanCSX</h1>
             <h4>Full-Stack Developer</h4>
             <div className='contacts'>
-                <button className='email'><img src={email_icon} /> Email</button>
-                <button className='linkedin'><img src={linkedin_icon} /> LinkedIn</button>
+                <button className='email' onClick={mailHandler}><img src={email_icon} /> Email</button>
+                <button className='linkedin' onClick={linkedInHandler}><img src={linkedin_icon} /> LinkedIn</button>
             </div>
         </header>
     )
